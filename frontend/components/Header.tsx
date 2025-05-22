7fc3999d73826cf29a9f0681e76ff32fc079bd46
bayuramdhan50 <bayuramdhan50@gmail.com>
2025-05-22 21:20:48 +0700

    feat: Update Header component branding with new gradient styling for the logo

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -39,7 +39,7 @@ export default function Header() {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2 text-primary" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
               </svg>
-              <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary-dark">FarmEase</span>
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-green-700 font-bold">FarmEase</span>
             </Link>
           </div>
         </div>
